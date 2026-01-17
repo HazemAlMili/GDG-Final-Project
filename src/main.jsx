878a669f94ccf9dffe157df4eefcb264c39ec416
HazemAlMelli <hazemalmile7@gmail.com>
2026-01-17 20:27:00 +0200

    Fix React Router basename for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -17,7 +17,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/real-state/">
+  <BrowserRouter basename="/GDG-Final-Project/">
     <App />
   </BrowserRouter>
 );
